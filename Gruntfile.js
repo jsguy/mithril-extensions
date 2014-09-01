72da23333a02a758f95e46e1591097c9d1615f55
jsguy <mikkel@pointful.com>
2014-09-02 09:00:25 +1000

    Added build file and package

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+	//	Concatenation file order
+	var concatFiles = ['node_modules/mithril/mithril.js', '../mithril.sugartags/mithril.sugartags.js', '../mithril.bindings/mithril.bindings.js', '../mithril.templates/mithril.templates.js'];
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				//  We'd prefer to fail on missing files, but at least this will warn: https://github.com/gruntjs/grunt-contrib-concat/issues/15
+				nonull: true,
+				files: {
+					'dist/version/<%= pkg.name %>-<%= pkg.version %>.js': concatFiles,
+					'dist/<%= pkg.name %>.js': concatFiles
+				}
+			}
+		},
+		qunit: {
+			files: ['test/**/*.htm']
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'dist/version/<%= pkg.name %>-<%= pkg.version %>.min.js': 'dist/version/<%= pkg.name %>-<%= pkg.version %>.js',
+					'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js'
+				}
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				ignores: ['src/template/_headsole.js', 'src/template/_footsole.js', 'test/libs/*.js'],
+				// options here to override JSHint defaults
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				},
+				//	Ignore specific errors
+				'-W015': true,	//	Indentation of }
+				'-W099': true,	//	Mixed spaces and tabs
+				'-W032': true	//	Unnecessary semicolon
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			//	Just build when watching
+			tasks: ['concat']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('test', ['jshint', 'qunit']);
+	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+	grunt.registerTask('justbuild', ['concat', 'uglify']);
+};
